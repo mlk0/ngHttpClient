6615c40accdd37164b17c40bc65d87521dcf5d6a
m1k0 <acme@gmx.com>
2018-02-18 10:23:41 -0500

    added manual subscription that parses the result and forms another type of array that is bound to template

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,27 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import {delay} from 'rxjs/operators'
+import { delay } from 'rxjs/operators'
 // import 'rxjs/operator/delay'
 // import 'rxjs/operators/delay'
- 
+
 import { JsonPlaceHoldService } from '../services/json-place-hold.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
 
+  postSubscription: Subscription;
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
+  }
   posts$: Observable<any>;
+
+  postsIDsandTitles: Object[];
+
+
   constructor(
-    private jsonService : JsonPlaceHoldService
+    private jsonService: JsonPlaceHoldService
   ) { }
 
 
   ngOnInit() {
-     this.posts$ = this.jsonService.getPosts().pipe(delay(5000));
+    this.posts$ = this.jsonService.getPosts().pipe(delay(5000));
 
+  }
+
+  getPostsIdAndTitle() {
+   this.postSubscription = this.jsonService.getPosts().subscribe(s => {
+
+      let posts = [];
+      //if we knot that s is and array
+      (<Object[]>s).forEach(i => {
+        let p = {
+          PostId: i["id"],
+          PostTitle: i["title"]
+        }
+        posts.push(p);
+      });
+
+      this.postsIDsandTitles = posts;
+    }
+
+    )
   }
 
 }
